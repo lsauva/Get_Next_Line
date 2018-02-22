d90548279a42d28ac7e428c0adf6a7f52a548c87
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-02-22 16:47:44 +0100

    initial commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,15 @@
 /*   By: lsauvage <lsauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/07 18:41:03 by lsauvage          #+#    #+#             */
-/*   Updated: 2018/02/07 18:42:28 by lsauvage         ###   ########.fr       */
+/*   Updated: 2018/02/22 16:46:53 by lsauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include "libft.h"
+
+int get_next_line(const int fd, char **line)
+{
+    static t_list   *file;
+    t_list          line;
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,16 @@
 /*   By: lsauvage <lsauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/07 18:41:00 by lsauvage          #+#    #+#             */
-/*   Updated: 2018/02/07 19:32:02 by lsauvage         ###   ########.fr       */
+/*   Updated: 2018/02/22 16:45:17 by lsauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+
+# define BUFF_SIZE 50
+# define EOL '\n'
+int     get_next_line(const int fd, char **line);
+
+#endif
